937c6f400b1bf8f3820cfcaa1b4625ab7bf8c3ab
Tom Smithrosser <tom@Toms-MacBook-Pro.local>
2020-08-02 22:10:31 +0100

    add edge cases for bouncing_ball

--- src/bouncing_ball.cpp
+++ src/bouncing_ball.cpp
@@ -4,7 +4,12 @@ int bouncing_ball(double h, double bounce, double window)
 {
     // ball dropped from height, bounces a fraction of the height
     // every bounce, check whether height >= window
-    
+
+    if (h < 1 || bounce <= 0 || bounce >= 1 || window >= h)
+    {
+        return -1;
+    }
+
     int sightings = 0;
     
     while(h >= window)
